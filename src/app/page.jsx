2fd5f2bc5f208b3adc32e2de26ce75a3c9ad8ca5
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2026-02-09 20:08:39 +0100

    title style

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="min-h-screen min-w-screen grid place-items-center">
-      <h1 className="text-4xl font-extrabold text-blue-800">Escuela 2026</h1>
+
+      <h1 className="p-6 w-5/6 text-center text-4xl font-extrabold text-indigo-800  bg-slate-300/30 rounded-full border border-slate-300">
+        Escuela 2026
+      </h1>
 
       <div className="flex flex-col gap-4 font-bold text-white">
         <Link
